d68d2c40254c0f5fe881c0b4ac479d7a39ae6292
jmaczan <jedrzejpawel@maczan.pl>
2024-02-15 23:10:48 +0100

    Convolution applied with success

--- src/tinyconv.py
+++ src/tinyconv.py
@@ -2,22 +2,23 @@ import numpy as np
 from PIL import Image
 
 
-def convolution(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
-    horizontal = int((kernel.shape[0] - 1)/2)
-    vertical = int((kernel.shape[1] - 1)/2)
+def tinyconv(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    image_height, image_width, rgb_channels = image.shape
+    kernel_width = int((kernel.shape[0] - 1)/2)
+    kernel_height = int((kernel.shape[1] - 1)/2)
     output = np.copy(image)
 
+    for channel in range(rgb_channels): 
+        for vertical_index in range(kernel_height, image_height - kernel_height):
+            for horizontal_index in range(kernel_width, image_width - kernel_width):
+                pixel_surrounding = image[vertical_index-kernel_height:vertical_index+kernel_height+1, horizontal_index-kernel_width:horizontal_index+kernel_width+1, channel]
+                output[vertical_index, horizontal_index, channel] = np.sum(pixel_surrounding * kernel)
 
-    iterator = np.nditer(image, flags=['multi_index'])
-    for _ in iterator:
-        pixel_surrounding = image[iterator.multi_index[0]:iterator.multi_index[0]+horizontal, iterator.multi_index[1]:iterator.multi_index[1]+vertical]
-        output[iterator.multi_index] = np.sum(pixel_surrounding * kernel)
-        
     return output
 
 if __name__ == "__main__":
     image = np.array(Image.open('kirby.png').convert('RGB'))
     kernel = np.full(9, 1/9).reshape(3,3)
     
-    image = Image.fromarray(convolution(image, kernel)).convert('RGB')
+    image = Image.fromarray(tinyconv(image, kernel)).convert('RGB')
     image.save("output.png")
